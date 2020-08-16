c80b148dd7e320500e4b9e392bce8ed04dcd9e6c
Ubuntu <ubuntu@ip-172-31-10-100.ap-northeast-1.compute.internal>
2020-08-16 10:11:40 +0000

    articleのmodelを作成

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
     def index
-        render 'home/index'
-    end 
+        @article = Article.first
+    end  
     
     def about
         render 'home/about'
